d63254e39d194763205885d34ad1ba1777a8ba84
Josias Neto <josiasnieto@hotmail.com>
2024-08-14 17:09:02 +0200

    Game finished

--- game.py
+++ game.py
@@ -40,16 +40,16 @@ class Bird:
         self.speed  = 0
         self.frames = 0
         self.height = self.y
-        self.image  = IMAGES[0]
+        self.image  = self.IMAGES[0]
 
     # Jump Method #
     def jump(self):
         self.time   = 0
-        self.speed  = -10.5
+        self.speed  = -20.5
         self.height = self.y
 
     # Movement Method #
-    def movement(self):
+    def move(self):
         self.time += 1
         self.aceleration = 1.5
 
@@ -93,13 +93,152 @@ class Bird:
 
         # draw image 
         center_position = self.image.get_rect(topleft = (self.x, self.y)).center
-        rotate_image    = pygame.transform.rotate(self.imagem, self.angle)
+        rotate_image    = pygame.transform.rotate(self.image, self.angle)
         hitbox          = rotate_image.get_rect(center = center_position)
 
         screen.blit(rotate_image, hitbox.topleft)
 
+    def mask(self):
+        return pygame.mask.from_surface(self.image)
+
 class Pipe:
-    pass
+    SPEED    = 5
+    DISTANCE = 200
+
+    def __init__(self, x):
+        self.x               = x
+        self.height          = 0
+        self.position_top    = 0
+        self.position_bottom = 0
+        self.passed          = False
+
+        self.PIPE_BOTTOM = IMG_PIPE
+        self.PIPE_TOP    = pygame.transform.flip(IMG_PIPE, False, True)
+
+        self.set_height()
+
+    def set_height(self):
+        self.height          = random.randrange(50, 450)
+        self.position_top    = self.height - self.PIPE_TOP.get_height()
+        self.position_bottom = self.height + self.DISTANCE
+
+    def move(self):
+        self.x -= self.SPEED
+    
+    def draw(self, screen):
+        screen.blit(self.PIPE_TOP, (self.x, self.position_top))
+        screen.blit(self.PIPE_BOTTOM, (self.x, self.position_bottom))
+
+    def collision(self, bird):
+        bird_mask    = bird.mask()
+        mask_top     = pygame.mask.from_surface(self.PIPE_TOP)
+        mask_bottom  = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        distance_top    = (self.x - bird.x, self.position_top - round(bird.y))
+        distance_bottom = (self.x - bird.x, self.position_bottom - round(bird.y))
+
+        point_top    = bird_mask.overlap(mask_top, distance_top)
+        point_bottom = bird_mask.overlap(mask_bottom, distance_bottom)
+
+        if point_bottom or point_top:
+            return True
+        else:
+            return False
 
 class Floor:
-    pass
+    SPEED = 5
+    WIDTH = IMG_FLOOR.get_width()
+    IMAGE = IMG_FLOOR
+
+    def __init__(self, y):
+        self.y  = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.SPEED
+        self.x2 -= self.SPEED
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+    
+    def draw(self, screen):
+        screen.blit(self.IMAGE, (self.x1, self.y))
+        screen.blit(self.IMAGE, (self.x2, self.y))
+
+def draw_screen(screen, birds, pipes, floor, points):
+
+    screen.blit(IMG_BACKGROUND, (0, 0))
+
+    for bird in birds:
+        bird.draw(screen)
+
+    for pipe in pipes:
+        pipe.draw(screen)
+
+    text = FONT.render(f"Points: {points}", 1, (255, 255, 255))
+    screen.blit(text, (WIDTH - 10 - text.get_width(), 10))
+    floor.draw(screen)
+    pygame.display.update()
+
+def main():
+    birds  = [Bird(230, 350)]
+    floor  = Floor(730)
+    pipes  = [Pipe(700)]
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    points = 0
+    clock  = pygame.time.Clock()
+
+    is_running = True
+
+    while is_running:
+
+        clock.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    for bird in birds:
+                        bird.jump()
+        
+        for bird in birds:
+            bird.move()
+        floor.move()
+
+        create_pipe = False
+        remove_pipe = []
+
+        for pipe in pipes:
+            for i, bird in enumerate(birds):
+                if pipe.collision(bird):
+                    birds.pop(i)
+                if not pipe.passed and bird.x > pipe.x:
+                    pipe.passed = True 
+                    create_pipe = True
+                pipe.move()
+
+                if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                    remove_pipe.append(pipe)
+        
+        if create_pipe:
+            points += 1
+            pipes.append(Pipe(600))
+        
+        for pipe in remove_pipe:
+            pipes.remove(pipe)
+
+        for i, bird in enumerate(birds):
+            if (bird.y + bird.image.get_height()) > floor.y or bird.y < 0:
+                birds.pop(i)
+
+        draw_screen(screen, birds, pipes, floor, points)
+
+
+if __name__ == '__main__':
+    main()
